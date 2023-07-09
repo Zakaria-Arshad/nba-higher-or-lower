facb4809d87a07f3137c2c38fe3b8304872199e4
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-07-09 16:35:32 -0400

    Initial commit

---
+++ src/main/java/com/example/nbagame/NBAGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nbagame/NBAGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

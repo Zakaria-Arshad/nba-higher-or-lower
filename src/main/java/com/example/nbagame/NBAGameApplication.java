b642b7388a6deb0ec792a7d8639af57a3ca81532
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-07-09 17:27:04 -0400

    Initial commit

--- src/main/java/com/example/nbagame/NBAGameApplication.java
+++ src/main/java/com/example/nbagame/NBAGameApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NbaGameApplication {
+public class NBAGameApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NbaGameApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NBAGameApplication.class, args);
+    }
 
 }
